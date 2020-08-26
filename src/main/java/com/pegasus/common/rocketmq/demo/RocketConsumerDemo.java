c3f8ecb0ac005db703260a1a3c7b6345e40c3f45
unwaychen <unwaychen@gmail.com>
2020-08-26 18:05:27 +0800

    [ADD]增加AwsS3访问CEPH客户端

--- src/main/java/com/pegasus/common/rocketmq/demo/RocketConsumerDemo.java
+++ src/main/java/com/pegasus/common/rocketmq/demo/RocketConsumerDemo.java
@@ -2,7 +2,9 @@ package com.pegasus.common.rocketmq.demo;
 
 import com.pegasus.common.rocketmq.annotation.RocketMQConsumeListener;
 import com.pegasus.common.rocketmq.core.RocketMQListener;
+import com.pegasus.common.rocketmq.core.RocketMQPushConsumerLifecycleListener;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,9 +13,24 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RocketMQConsumeListener(topic = "test", tag = "*", consumerGroup = "pegasus_demo_222")
-public class RocketConsumerDemo implements RocketMQListener<MdpHrDataUpdateVo> {
+public class RocketConsumerDemo implements RocketMQListener<MdpHrDataUpdateVo>, RocketMQPushConsumerLifecycleListener {
+     /**
+       * @Author: enHui.Chen
+       * @Description: 处理数据
+       * @Data 2020/8/26
+       */
     @Override
     public void onConsumeMessage(MdpHrDataUpdateVo message) {
         log.info("MdpHrDataUpdate:{}", message.toString());
+    }
+
+     /**
+       * @Author: enHui.Chen
+       * @Description: 启动前
+       * @Data 2020/8/26
+       */
+    @Override
+    public void prepareStart(DefaultMQPushConsumer consumer) {
+
     }
 }
---
+++ src/main/java/com/pegasus/test/awss3/AwsS3FileService.java
@@ -0,0 +1,7 @@
+package com.pegasus.test.awss3;
+
+/**
+ * Created by enHui.Chen on 2020/8/26.
+ */
+public class AwsS3FileService {
+}
